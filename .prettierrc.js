40f3dc824c77d1b10c4bc35381bddb5bc8ddcfab
DESKTOP-OCF9DDS\Wang <seonghyeogwang@gmail.com>
2026-05-12 23:39:23 +0900

    codinglist 수정

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,6 +4,15 @@ module.exports = {
     plugins: ['prettier-plugin-sort-json'],
     jsonRecursiveSort: true,
     jsonSortOrder: JSON.stringify(jsonSortOrder),
+    overrides: [
+        {
+            files: ['markup/_coding_list/coding_list.json', 'dist/_coding_list/coding_list.json'],
+            options: {
+                jsonRecursiveSort: false,
+                jsonSortOrder: '',
+            },
+        },
+    ],
     arrowParens: 'avoid', // 화살표 함수 괄호 사용 방식
     bracketSpacing: true, // 객체 리터럴에서 괄호에 공백 삽입 여부
     endOfLine: 'auto', // EoF 방식, OS별로 처리 방식이 다름
